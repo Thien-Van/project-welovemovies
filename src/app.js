9db82c8ae6fe91a53c45550ea939348ebee327c6
Ting <thienvan.to@gmail.com>
2021-06-12 16:14:18 +0200

    added error functions and updated app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,15 @@
 if (process.env.USER) require("dotenv").config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
+
+const moviesRouter = require("./movies/movies.router");
+const reviewsRouter = require("./reviews/reviews.router");
+const theatersRouter = require("./theaters/theaters.router");
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/methodNotAllowed");
+
+app.use(cors());
+app.use(express.json());
 
 module.exports = app;
