57c0919a52da32c62025d42cec5670d1fb31172a
Ting <thienvan.to@gmail.com>
2021-06-15 17:59:35 +0200

    implemented all queries for movies

--- src/reviews/reviews.service.js
+++ src/reviews/reviews.service.js
@@ -4,7 +4,26 @@ function list(movieId) {
   return knex("reviews as r")
     .join("critics as c", "r.critic_id", "c.critic_id")
     .select("r.*", "c.* as critic")
-    .where("r.movie_id", movieId);
+    .where("r.movie_id", movieId)
+    .then((data) => {
+      const restructuredData = data.map((review) => {
+        const critic = {
+          preferred_name: review.preferred_name,
+          surname: review.surname,
+          organization_name: review.organization_name,
+        };
+        const { review_id, content, score, critic_id, movie_id } = review;
+        return {
+          review_id,
+          content,
+          score,
+          critic_id,
+          movie_id,
+          critic,
+        };
+      });
+      return restructuredData;
+    });
 }
 
 module.exports = {
